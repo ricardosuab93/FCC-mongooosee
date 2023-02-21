18dd403e3d2056e7a7ebf6e4d08dff2f5d4adf31
Ricardo Suarez <ricardo_suarez93@hotmail.com>
2023-02-20 23:07:25 -0500

    FIRST COMMIT

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+var personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+})
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
